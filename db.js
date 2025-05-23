b7c119971174d98cecd2553dee9144a44b795311
Umeshguat <92910458+Umeshguat@users.noreply.github.com>
2025-05-23 11:13:42 +0530

    Db Credential update

--- db.js
+++ db.js
@@ -1,33 +1,14 @@
-// // db.js
-// import mysql from 'mysql';
 
-// const db = mysql.createConnection({
-//   host: '51.79.204.225',
-//   port : 3306,
-//   user: 'corecad_vhingo',
-//   password: '123@Vhingo',
-//   database: 'corecad_vhingo'
-// });
-
-// db.connect((err) => {
-//   if (err) {
-//     console.error('Error connecting to MySQL database:', err);
-//     return;
-//   }
-//   console.log('Connected to MySQL database');
-// });
-
-// export default db;
 
 
 import mysql from 'mysql';
 
 const db = mysql.createConnection({
-  host: '127.0.0.1',
+ host: '150.242.200.29',
   port : 3306,
-  user: 'root',
-  password: '',
-  database: 'prime_hill'
+  user: 'qubeta_prism',
+  password: '=szPo_2icWop',
+  database: 'qubeta_prism'
 });
 
 db.connect((err) => {
